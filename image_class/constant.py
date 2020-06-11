5a5af3bd7203763318aec8dd53d223675c0c1124
shigasy <kxx555u.u@gmail.com>
2020-06-11 14:16:48 +0900

    add: lossと正答率

--- image_class/constant.py
+++ image_class/constant.py
@@ -1,5 +1,5 @@
 # .npyはNumpy配列
 # 大量の画像をNumpy配列に変換して使う
 FILE_NAMES = ["img1.npy", "img2.npy"]
-CLASS_NAMES = ["カツオ", "マグロ"]
+CLASS_NAMES = ["マグロ", "マグロの刺し身"]
 hw = {"height": 32, "width": 32}
--- image_class/main.py
+++ image_class/main.py
@@ -6,7 +6,7 @@ def main():
     """
     learn_scheduleが大きいほど、エポック数による学習率を減らす割合が小さくなる
     """
-    learning(tsnum=10, nb_epoch=100, batch_size=8, learn_schedule=0.9)
+    learning(tsnum=10, nb_epoch=100, batch_size=8, learn_schedule=0.97)
     # learning(tsnum=10, nb_epoch=100, batch_size=8, learn_schedule=0.9)
 
 
